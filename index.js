df9acf97e0a0eba71d1715bb38d18a05c6c4aa1a
navasco <53056349+navascogt@users.noreply.github.com>
2023-10-17 19:38:54 +0000

    add yaml

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const fs = require('fs')
 const path = require('path')
 
 const redirectsFile = fs.readFileSync(path.join(__dirname, 'redirects.yml'), 'utf-8')
+const redirect = YAML.parse(redirectsFile)
+
 console.log(redirectsFile)
 
 // 2. Generate an html page for each redirect ur; from template.jtml
