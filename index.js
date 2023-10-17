69c40e4ddaf8f2abb17f20521d5cf90b03971335
navasco <53056349+navascogt@users.noreply.github.com>
2023-10-17 19:35:50 +0000

    complete read

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // 1. Load all the urls from redirects.yml
 const YAML = require('yaml')
+const fs = require('fs')
+// For Windows
+const path = require('path')
 
+const redirectsFile = fs.readFileSync(path.join(__dirname, 'redirects.yml'), 'utf-8')
+console.log(redirectsFile)
 
 // 2. Generate an html page for each redirect ur; from template.jtml
